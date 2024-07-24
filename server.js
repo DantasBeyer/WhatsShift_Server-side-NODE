512a665fd732edd954a87f1073c47012a431ec1f
DantasBeyer <dantasbeyer@hotmail.com>
2024-07-24 21:29:28 +0200

    mongoose route created

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const bodyParser = require("body-parser");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-// server connection
+// Configuração do servidor
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -12,20 +12,17 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Conectar ao MongoDB
-mongoose.connect("mongodb://localhost:27017/whatsshift", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect('mongodb://localhost:27017/whatsshift');
 
 // Verificar conexão ao MongoDB
 const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("MongoDB database connection established successfully");
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
 });
 
 // Rotas
-const userProfileRouter = require("./routes/userProfile");
-app.use("/userProfile", userProfileRouter);
+const userProfileRouter = require('./routes/userProfile');
+app.use('/userProfile', userProfileRouter);
 
 // Iniciar o servidor
 app.listen(port, () => {
